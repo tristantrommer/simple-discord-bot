252325e5e92e45c290d03fc47f90db38c22d9614
Tristan Trommer <mail@tristantrommer.com>
2021-12-27 00:30:14 +0100

    Event handling

---
+++ events/interactionCreate.js
@@ -0,0 +1,19 @@
+module.exports = {
+    name: 'interactionCreate',
+    async execute(interaction) {
+        if (interaction.isCommand()) {
+            const command = interaction.client.commands.get(interaction.commandName);
+
+            if (!command) return;
+
+            try {
+                await command.execute(interaction);
+            } catch (error) {
+                console.error(error);
+                return interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+            }
+        }
+
+        return;
+    },
+};
--- index.js
+++ index.js
@@ -20,23 +20,14 @@ for (const file of commandFiles) {
     client.commands.set(command.data.name, command);
 }
 
-client.on('ready', () => {
-    console.log('simple-discord-bot is ready!');
-});
-
-client.on('interactionCreate', async interaction => {
-    if (!interaction.isCommand()) return;
-
-    const command = client.commands.get(interaction.commandName);
-
-    if (!command) return;
-
-    try {
-        await command.execute(interaction);
-    } catch (error) {
-        console.error(error);
-        return interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+for (const file of eventFiles) {
+    const event = require(`./events/${file}`);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args));
     }
-});
+}
 
 client.login(process.env.DISCORD_TOKEN);
