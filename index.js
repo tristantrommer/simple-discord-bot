e2d724aced5b873ffd75aae040e549b22a051d80
Tristan Trommer <mail@tristantrommer.com>
2021-12-27 00:07:39 +0100

    Command handling

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const { Client, Intents } = require('discord.js');
-require('dotenv').config()
+const { Client, Collection, Intents } = require('discord.js');
+const fs = require('fs');
+
+require('dotenv').config();
 
 const client = new Client({
     intents: [
@@ -10,16 +12,31 @@ const client = new Client({
     ]
 });
 
+client.commands = new Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.data.name, command);
+}
+
 client.on('ready', () => {
-    console.log('Bot is ready');
+    console.log('simple-discord-bot is ready!');
 });
 
-client.on('messageCreate', (message) => {
-    if (message.content === 'ping') {
-        message.reply({
-            content: 'pong'
-        });
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+
+    const command = client.commands.get(interaction.commandName);
+
+    if (!command) return;
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        return interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
     }
-})
+});
 
 client.login(process.env.DISCORD_TOKEN);
