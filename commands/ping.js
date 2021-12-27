e2664da8fe517c3c009cd9248e5a022d8dc4e391
Tristan Trommer <mail@tristantrommer.com>
2021-12-27 01:06:05 +0100

    Ping command

--- commands/ping.js
+++ commands/ping.js
@@ -1,10 +1,37 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
+const {MessageEmbed} = require("discord.js");
+
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('ping')
         .setDescription('Replies with Pong!'),
     async execute(interaction) {
-        await interaction.reply('Pong!');
+        const pingingEmbed = {
+            color: 0x0066ff,
+            title: 'Pinging...',
+        }
+
+        const sent = await interaction.reply({ embeds: [pingingEmbed], fetchReply: true });
+
+        const finalEmbed = {
+            color: 0x0066ff,
+            title: 'Pong!',
+            fields: [
+                {
+                    name: 'Websocket heartbeat',
+                    value: `${interaction.client.ws.ping}ms`,
+                    inline: true
+                },
+                {
+                    name: 'Roundtrip latency',
+                    value: `${sent.createdTimestamp - interaction.createdTimestamp}ms`,
+                    inline: true
+                }
+            ],
+            timestamp: new Date(),
+        }
+
+        await interaction.editReply({ embeds: [finalEmbed] });
     }
 };
